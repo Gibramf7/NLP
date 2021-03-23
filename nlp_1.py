b1cd0043eb5c0327e2b7f816089195f0ef536ccf
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-03-22 21:16:26 -0500

    third commit

--- nlp_1.py
+++ nlp_1.py
@@ -66,3 +66,50 @@ sentence = TextBlob('Ths sentance has missplled wrds.')
 corrected_sentence = sentence.correct()
 
 print(corrected_sentence)
+
+
+from textblob import Word
+
+word1 = Word('studies')
+word2 = Word('variety')
+
+print(word1.lemmatize())
+print(word2.lemmatize())
+
+happy = Word('happy')
+
+print(happy.definitions)
+
+'''
+for synset in happy.synsets:
+    print(synset)
+    for lemma in synset.lemmas():
+        print(lemma)
+        print(lemma.name())
+
+
+lemmas = happy.synsets[0].lemmas()
+print(lemmas)
+
+
+for lemma in lemmas[0].synsets.antonyms():
+    print(lemma.name())
+'''
+
+import nltk
+
+#nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+stops = stopwords.words("english")
+
+#print(stops)
+
+blob = TextBlob('Today is a beautiful day.')
+
+print(blob.words)
+
+cleanlist = [word for word in blob.words if word not in stops]
+
+print(cleanlist)
